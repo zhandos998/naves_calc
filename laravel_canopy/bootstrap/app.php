8380294951f315dbe7366867e7a7989598d21a95
zhandos998 <61329282+zhandos998@users.noreply.github.com>
2025-04-24 00:18:55 +0600

    Update app.php

--- laravel_canopy/bootstrap/app.php
+++ laravel_canopy/bootstrap/app.php
@@ -12,9 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+        // $middleware->api(prepend: [
+        //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        // ]);
 
         $middleware->alias([
             'role'  =>  \App\Http\Middleware\RoleMiddleware::class, // наш мидлвар роли
